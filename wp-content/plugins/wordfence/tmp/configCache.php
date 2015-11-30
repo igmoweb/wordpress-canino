e2a271496cb4f21e518b2bb67e4ffd052e1ef6fe
Ignacio Cruz <igmoweb@gmail.com>
2015-11-30 23:00:47 +0100

    .

--- wp-content/plugins/wordfence/tmp/configCache.php
+++ wp-content/plugins/wordfence/tmp/configCache.php
@@ -1,4 +1,4 @@
 <?php
 /* Wordfence temporary file security header */
 echo "Nothing to see here!\n"; exit(0);
-?>a:7:{s:9:"cacheType";s:0:"";s:9:"howGetIPs";s:0:"";s:6:"apiKey";s:160:"a37e1cfd1d87e1b50ec5d268c6292d9851ffd1d75d23317e0cd94a1cc3c74a1224c652a3153c0f0ca0ac3b839c60fb8b05b1e08d8e23d9d176a841e33bc4241e0df7a53337b46ff8a60cce1383c5d717";s:20:"loginSec_lockoutMins";s:3:"240";s:15:"firewallEnabled";s:1:"1";s:18:"liveTrafficEnabled";s:1:"1";s:14:"disableCookies";s:1:"0";}
+?>a:7:{s:9:"cacheType";s:0:"";s:9:"howGetIPs";s:0:"";s:6:"apiKey";s:160:"766335f146648de0c6698cd0a907d693ca97a7c11836f61def8f765b1c3e35dbd004f5a3a5ea89281edcb4938ea5a0b451858d6d64d9c481a6619a591750216dd35e4a9bcaaa6af71b8225d38fb4ecee";s:20:"loginSec_lockoutMins";s:3:"240";s:15:"firewallEnabled";s:1:"1";s:18:"liveTrafficEnabled";s:1:"1";s:14:"disableCookies";s:1:"0";}
